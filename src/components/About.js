2775f4a8b0d1b922ee0f6380e2bb9df7c4bdd5ac
metacryst <samrussell99@gmail.com>
2020-06-07 14:56:55 -0500

    fixed security warning

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 function About() {
   return (
     <>
-    <p className="aboutText">'['work in progress']'</p>
+    <p className="aboutText">['work in progress']</p>
     <div className="credit">
       <p>designed by Sam Russell and Andrés Ortiz Montalvo, 2020.</p>
       <a className="website" href="https://www.samrussell.com/" target="_blank" rel="noopener noreferrer">---> samrussell.com</a>
