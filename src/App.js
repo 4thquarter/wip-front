8e7bece099cd1b304a8852879701cf04b032ed20
metacryst <samrussell99@gmail.com>
2020-06-16 10:21:12 -0500

    idk at this point

--- src/App.js
+++ src/App.js
@@ -554,7 +554,7 @@ function App() {
 					dataVariable = data;
 					setPostId(data.id);
 					setUserId(data._id);
-
+					setError(false);
 					setIsUserFound(true);
 				} else {
 					// console.log('bad user');
