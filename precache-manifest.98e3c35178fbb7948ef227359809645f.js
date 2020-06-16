c9b5c04ad684d081713d5441e3ffc701ec76cd34
andresaugusto-ga <andres.augusto.ga@icloud.com>
2020-06-16 07:38:57 -0400

    Updates

---
+++ precache-manifest.98e3c35178fbb7948ef227359809645f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed9d1d2a4bcd4c57abdbc3b6ff3a79a",
+    "url": "/wip-front/index.html"
+  },
+  {
+    "revision": "a07914b19618a5abe16d",
+    "url": "/wip-front/static/css/main.71d2f6ce.chunk.css"
+  },
+  {
+    "revision": "e4a46582bcb147d11215",
+    "url": "/wip-front/static/js/2.ad94b396.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/wip-front/static/js/2.ad94b396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07914b19618a5abe16d",
+    "url": "/wip-front/static/js/main.60485d5c.chunk.js"
+  },
+  {
+    "revision": "89d7d71cbc0ede7708ff",
+    "url": "/wip-front/static/js/runtime-main.9b9f8315.js"
+  }
+]);
