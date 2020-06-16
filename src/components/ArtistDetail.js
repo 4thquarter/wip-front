0a64a87a85e4b0d9a21e421c53f69b4a79aa6d62
metacryst <samrussell99@gmail.com>
2020-06-16 08:42:13 -0500

    add a lil

--- src/components/ArtistDetail.js
+++ src/components/ArtistDetail.js
@@ -84,44 +84,44 @@ const ArtistDetail = ({ match }) => {
 			"-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
 	};
 
-	// if (deleted) {
-	// 	return <Redirect to='/pieces' />;
-	// }
-	// if (error) {
-	// 	return (
-	// 		<div style={entranceText}>
-	// 			Sorry, the artist is out on a spiritual retreat.
-	// 		</div>
-	// 	);
-	// }
+	if (deleted) {
+		return <Redirect to='/pieces' />;
+	}
+	if (error) {
+		return (
+			<div style={entranceText}>
+				Sorry, the artist is out on a spiritual retreat.
+			</div>
+		);
+	}
 
 
-	// if (!artist) {
-	// 	return (
-	// 		<div className='details'>
-	// 			<motion.h2
-	// 				className='loading'
-	// 				style={entranceText}
-	// 				animate={{
-	// 					color: [
-	// 						'#E02200',
-	// 						'#00B82D',
-	// 						'#2C3EAD',
-	// 						'#FA0',
-	// 						'#9500B8',
-	// 						'#E04A00',
-	// 					],
-	// 				}}
-	// 				transition={{
-	// 					type: 'tween',
-	// 					duration: 7,
-	// 					yoyo: Infinity,
-	// 				}}>
-	// 				☯☠♠<motion.span style={{ color: '#695F49' }}>LOADING</motion.span>♠☠☯
-	// 			</motion.h2>
-	// 		</div>
-	// 	);
-	// }
+	if (!artist) {
+		return (
+			<div className='details'>
+				<motion.h2
+					className='loading'
+					style={entranceText}
+					animate={{
+						color: [
+							'#E02200',
+							'#00B82D',
+							'#2C3EAD',
+							'#FA0',
+							'#9500B8',
+							'#E04A00',
+						],
+					}}
+					transition={{
+						type: 'tween',
+						duration: 7,
+						yoyo: Infinity,
+					}}>
+					☯☠♠<motion.span style={{ color: '#695F49' }}>LOADING</motion.span>♠☠☯
+				</motion.h2>
+			</div>
+		);
+	}
 
 	return (
 		<div className='details'>
